804eaa891794868d93391b518dbb3a177c709d92
SND\jordanpa_cp <SND\jordanpa_cp@81e6e737-5a20-4d0f-a97c-cb01297b46e0>
2006-10-03 17:58:06 +0000

    Adds planar, spherical, and cylindrical texture coordinate generation routines. See the new sample for usage tips and how to use them with databinding.
    git-svn-id: https://3dtools.svn.codeplex.com/svn@7406 81e6e737-5a20-4d0f-a97c-cb01297b46e0

--- 3DTools/3DTools/MathUtils.cs
+++ 3DTools/3DTools/MathUtils.cs
@@ -309,6 +309,87 @@ namespace _3DTools
             return to2D;
         }
 
+        /// <summary>
+        ///     Transforms the axis-aligned bounding box 'bounds' by
+        ///     'transform'
+        /// </summary>
+        /// <param name="bounds">The AABB to transform</param>
+        /// <returns>Transformed AABB</returns>
+        public static Rect3D TransformBounds(Rect3D bounds, Matrix3D transform)
+        {
+            double x1 = bounds.X;
+            double y1 = bounds.Y;
+            double z1 = bounds.Z;
+            double x2 = bounds.X + bounds.SizeX;
+            double y2 = bounds.Y + bounds.SizeY;
+            double z2 = bounds.Z + bounds.SizeZ;
+
+            Point3D[] points = new Point3D[] {
+                new Point3D(x1, y1, z1),
+                new Point3D(x1, y1, z2),
+                new Point3D(x1, y2, z1),
+                new Point3D(x1, y2, z2),
+                new Point3D(x2, y1, z1),
+                new Point3D(x2, y1, z2),
+                new Point3D(x2, y2, z1),
+                new Point3D(x2, y2, z2),
+            };
+
+            transform.Transform(points);
+
+            // reuse the 1 and 2 variables to stand for smallest and largest
+            Point3D p = points[0];
+            x1 = x2 = p.X; 
+            y1 = y2 = p.Y; 
+            z1 = z2 = p.Z;
+
+            for(int i = 1; i < points.Length; i++)
+            {
+                p = points[i];
+
+                x1 = Math.Min(x1, p.X); y1 = Math.Min(y1, p.Y); z1 = Math.Min(z1, p.Z);
+                x2 = Math.Max(x2, p.X); y2 = Math.Max(y2, p.Y); z2 = Math.Max(z2, p.Z);
+            }
+
+            return new Rect3D(x1, y1, z1, x2 - x1, y2 - y1, z2 - z1);
+        }
+
+        /// <summary>
+        ///     Normalizes v if |v| > 0.
+        /// 
+        ///     This normalization is slightly different from Vector3D.Normalize. Here
+        ///     we just divide by the length but Vector3D.Normalize tries to avoid
+        ///     overflow when finding the length.
+        /// </summary>
+        /// <param name="v">The vector to normalize</param>
+        /// <returns>'true' if v was normalized</returns>
+        public static bool TryNormalize(ref Vector3D v)
+        {
+            double length = v.Length;
+
+            if (length != 0)
+            {
+                v /= length;
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        ///     Computes the center of 'box'
+        /// </summary>
+        /// <param name="box">The Rect3D we want the center of</param>
+        /// <returns>The center point</returns>
+        public static Point3D GetCenter(Rect3D box)
+        {
+            return new Point3D(box.X + box.SizeX / 2, box.Y + box.SizeY / 2, box.Z + box.SizeZ / 2);
+        }
+
         public static readonly Matrix3D ZeroMatrix = new Matrix3D(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+        public static readonly Vector3D XAxis = new Vector3D(1, 0, 0);
+        public static readonly Vector3D YAxis = new Vector3D(0, 1, 0);
+        public static readonly Vector3D ZAxis = new Vector3D(0, 0, 1);
     }
 }
