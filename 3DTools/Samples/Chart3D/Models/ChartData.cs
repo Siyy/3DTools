ff1af4abdae444eb14f0077627739388c1116a1c
久永 <jiuyong@hotmail.com>
2020-07-20 16:48:11 +0800

    添加图表数据包装。

---
+++ 3DTools/Samples/Chart3D/Models/ChartData.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Jiuyong
+{
+	public partial class ChartData
+	{
+		public readonly int Width;
+		public readonly int Height;
+		//public readonly double[] Xs;
+		//public readonly double[] Ys;
+		public readonly double[,] Zs;
+		//public readonly double[] Zs;
+
+		//V -1
+		//public ChartData(
+		//	(double min, double max, double step) x
+		//	, (double min, double max, double step) y
+		//	, Func<double, double, double> f)
+		//{
+		//	var w = (x.max - x.min) / x.step;
+		//	Width =  Math.Abs((int)w);
+
+		//	var h = (y.max - y.min) / y.step;
+		//	Height = Math.Abs((int)h);
+
+		//	Zs = new double[Width, Height];
+
+		//	for (int i = 0; i < x.max; i+=x.step)
+		//	{
+		//		for (double j = 0; j < y.max; j+=y.step)
+		//		{
+		//			Zs[i, j] = f(i, j);
+		//		}
+		//	}
+		//}
+
+		//V 0
+		public ChartData(
+			int width
+			, int height
+			, Func<int, int, double> f)
+		{
+			Width = width;
+			Height = height;
+
+			Zs = new double[Width , Height];
+			//Zs = new double[Width * Height];
+
+			for (int i = 0; i < width; i++)
+			{
+				for (int j = 0; j < height; j++)
+				{
+					Zs[i , j] = f(i, j);
+					//Zs[i * width + j] = f(i, j);
+				}
+			}
+		}
+	}
+}
+namespace Jiuyong
+{
+	using System.Windows.Media.Media3D;
+	public partial class ChartData
+	{
+		public IEnumerable<Point3D> GetPositions()
+		{
+			for (int i = 0; i < Width; i++)
+			{
+				for (int j = 0; j < Height; j++)
+				{
+					yield return new Point3D(i, j, Zs[i, j]);
+				}
+			}
+		}
+
+		public IEnumerable<int> GetTriangleIndices()
+		{
+			for (int i = 0; i < Width-1; i++)
+			{
+				for (int j = 0; j < Height-1; j++)
+				{
+					var x0 = i * Width + j;
+
+					yield return x0;
+					yield return x0 + Width;
+					yield return x0 + 1;
+
+					yield return x0 + Width;
+					yield return x0 + Width + 1;
+					yield return x0 + 1;
+				}
+			}
+		}
+	}
+}
